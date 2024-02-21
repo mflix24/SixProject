6da23bebb86414d4f133095d59fad4183bd0afa0
mflix24 <banglacourse1223@gmail.com>
2024-02-21 16:34:32 +0600

    code changed for better performance.

--- BlogGeneration/bloggeneration.py
+++ BlogGeneration/bloggeneration.py
@@ -2,13 +2,10 @@ import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 
-## Creating a function for getting response from LLAma2 model
-
+## Creating a function for calling LLAma2 model through  CTransformers() class
 def getLLamaresponse(input_text,no_words,blog_style):
-
-    ### LLama2 model
+    ### creating an object of CTransformers() class
     llm=CTransformers(
-        # model='models/llama-2-7b-chat.ggmlv3.q8_0.bin', TheBloke/Llama-2-7B-Chat-GGML
         model='TheBloke/Llama-2-7B-Chat-GGML',
         model_type='llama',
         config={
@@ -16,52 +13,53 @@ def getLLamaresponse(input_text,no_words,blog_style):
             'temperature':0.01
             })
     
-    ## Prompt Template
-
+    ## template creating for the systm
     template="""
-        Write a blog for {blog_style} job profile for a topic {input_text}
+        Write a blog for {blog_style} job profile
+        for a topic {input_text}
         within {no_words} words.
-            """
-    
+        """
+    # Calling PromptTemplate() class and making an object
     prompt=PromptTemplate(
         input_variables=["blog_style","input_text",'no_words'],
         template=template
         )
     
-    ## Generate the ressponse from the LLama 2 model
+    # Generate the response from the object as llm through prompt.format() function
     response=llm(
         prompt.format(
             blog_style=blog_style,
             input_text=input_text,
             no_words=no_words
             ))
-    # print(response)
     return response
 
-
-
-
-
-# 
-st.set_page_config(page_title="Generate Blogs",
-                    page_icon='🤖',
-                    layout='centered',
+# Streamlit UI designing  by calling as set_page_config() function through streamlit as st
+st.set_page_config(page_title="Generate Blogs",     # for the tile
+                    page_icon='🤖',                 # for icon
+                    layout='centered',              # for layout
                     initial_sidebar_state='collapsed')
 
-st.header("Generate Blogs 🤖")
 
+# streamlit header
+st.header("ChatBot For Generate Blogs 🤖")
+
+# where user giving their input by text_input() function through streamlit
 input_text=st.text_input("Enter the Blog Topic")
 
-## creating to more columns for additonal 2 fields
 
+# creating two more columns for additonal 2 fields as no_words and blog_style
 col1,col2=st.columns([5,5])
-
 with col1:
     no_words=st.text_input('No of Words')
 with col2:
-    blog_style=st.selectbox('Writing the blog for',
-                            ('Researchers','Data Scientist','Common People'),index=0)
-    
+    blog_style=st.selectbox(
+        'Writing the blog for',
+        ('Researchers','Data Scientist','Common People'),
+        index=0
+        )
+
+# streamlit button caption='Generate'
 submit=st.button("Generate")
 
 ## Final response
